0c3aa3f51aa84033a5c8889c8800f1e21c7217ad
Nikola Jevric <46942531+NJevric@users.noreply.github.com>
2020-04-25 21:05:56 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<link rel="stylesheet" type="text/css" href="css/style404.css?<?php echo time(); ?>"/>
+	<title>CSA 404 page</title>
+	<link href="https://fonts.googleapis.com/css?family=Muli|Righteous&display=swap" rel="stylesheet"/>
+	<link rel="icon" type="image/x-icon" href="img/ikona.ico" /> 
+</head>
+<body>
+	<div id="custom">
+		<img src="img/logoHeader.png" alt="csa logo" class="centar">
+		<h1 class="centar">Page not found</h1>
+		<p class="centar">Sorry, we can't seem to find the page you were looking for</p>
+		<p class="centar">Take me back to <span><a class="centar" href="index.php"> Homepage</a></span></p>
+	</div>
+</body>
+</html>
